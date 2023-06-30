055ca0f15f2f4b549a6133a0db42512836afb6eb
ricardobar96 <ricardobar96@gmail.com>
2023-06-30 23:05:43 +0100

    Added functional project

---
+++ src/script.js
@@ -0,0 +1,180 @@
+import React from 'https://esm.sh/react@18.2.0'
+import ReactDOM from 'https://esm.sh/react-dom@18.2.0'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.plusBreak = this.plusBreak.bind(this);
+    this.minusBreak = this.minusBreak.bind(this);
+    this.plusSession = this.plusSession.bind(this);
+    this.minusSession = this.minusSession.bind(this);
+    this.reset = this.reset.bind(this);
+    this.start = this.start.bind(this);
+    this.playAudio = this.playAudio.bind(this);
+    this.stopAudio = this.stopAudio.bind(this);
+    this.state = {
+      break: 5,
+      session: 25,
+      minutes: 25,
+      seconds: 0,
+      label: 'Session',
+      running: true
+    };
+  }
+  
+  reset() {
+    this.setState({
+      break : 5,
+      session: 25,
+      minutes: 25,
+      seconds: 0,
+      label: "Session",
+      running: true
+    }); 
+    this.stopAudio();
+  }
+  
+  playAudio() {
+    document.getElementById("beep").play();
+  }
+  
+  stopAudio() {
+    document.getElementById("beep").pause();
+    document.getElementById("beep").currentTime = 0;
+  }
+  
+  start() { 
+    this.setState({
+      running: !this.state.running
+    });
+
+    const interval = setInterval(() => {
+      if(this.state.running === false) {
+        
+        if(this.state.seconds > 0) {
+          this.setState({
+            seconds : this.state.seconds - 1
+          }); 
+        }
+        
+        if(this.state.seconds == 0 && this.state.minutes > 0) {
+          this.setState({
+            minutes : this.state.minutes - 1,
+            seconds: 59
+          }); 
+        }
+        
+        if(this.state.seconds == 0 && this.state.minutes == 0 && this.state.label == "Session") {
+          const timeEnd = setTimeout(() => {
+            this.setState({
+            label: "Break",
+            minutes: this.state.break,
+            seconds: 0
+          }); 
+          this.playAudio();
+          }, 1000);
+        }
+        
+        if(this.state.seconds == 0 && this.state.minutes == 0 && this.state.label == "Break") {
+          const timeEndB = setTimeout(() => {
+            this.setState({
+            label: "Session",
+            minutes: this.state.session,
+            seconds: 0
+          }); 
+          this.playAudio();
+          }, 1000);
+        }
+      }
+      
+      if(this.state.running === true) {
+        clearInterval(interval);
+      } 
+    }, 1000);
+    return () => clearInterval(interval);    
+  }
+  
+  plusBreak() {
+    if(this.state.break <= 59 && this.state.running == true && this.state.label == "Break") {
+      this.setState({
+        break : this.state.break + 1,
+        minutes: this.state.break + 1,
+        seconds: 0
+       }); 
+     }
+    
+    if(this.state.break <= 59 && this.state.running == true && this.state.label != "Break") {
+      this.setState({
+        break : this.state.break + 1
+       }); 
+     }
+  }
+  
+  minusBreak() {
+    if(this.state.break >= 2 && this.state.running == true && this.state.label == "Break") {
+      this.setState({
+        break : this.state.break - 1,
+        minutes: this.state.break - 1,
+        seconds: 0
+       }); 
+     }
+    
+    if(this.state.break >= 2 && this.state.running == true && this.state.label != "Break") {
+      this.setState({
+        break : this.state.break - 1
+      }); 
+    }
+  }
+  
+  minusSession() {
+    if(this.state.session >= 2 && this.state.running == true && this.state.label == "Session") {
+      this.setState({
+        session : this.state.session - 1,
+        minutes: this.state.session - 1,
+        seconds: 0
+      }); 
+    }
+    
+    if(this.state.session >= 2 && this.state.running == true && this.state.label != "Session") {
+      this.setState({
+        session : this.state.session - 1
+      }); 
+    }
+  }
+    
+    plusSession() {
+      if(this.state.session <= 59 && this.state.running == true && this.state.label == "Session") {
+        this.setState({
+          session : this.state.session + 1,
+          minutes: this.state.session + 1,
+          seconds: 0
+        }); 
+      }
+      
+      if(this.state.session <= 59 && this.state.running == true && this.state.label != "Session") {
+      this.setState({
+        session : this.state.session + 1
+      }); 
+    }
+    }
+  
+  render() {
+    return (
+      <div id="clock">
+        <audio id="beep" src="http://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/music/start.ogg"></audio>
+        <h1>25 + 5 Clock</h1>
+        <div id="tags-container">
+          <h2 id="break-label">Break Lenght<p><button  id="break-decrement" onClick={this.minusBreak}>-</button><span id="break-length">{this.state.break}</span><button  id="break-increment" onClick={this.plusBreak}>+</button></p></h2>
+          <h2 id="session-label">Session Lenght<p><button  id="session-decrement" onClick={this.minusSession}>-</button><span id="session-length">{this.state.session}</span><button  id="session-increment" onClick={this.plusSession}>+</button></p></h2>
+        </div>
+        <div id="session-container">
+          <h1 id="timer-label">{this.state.label}</h1>
+          <h1 id="time-left">{this.state.minutes < 10 ? '0' + this.state.minutes : this.state.minutes}:{this.state.seconds < 10 ? '0' + this.state.seconds : this.state.seconds}</h1>
+          <p><button id="start_stop" onClick={this.start}>Start</button><button id="reset" onClick={this.reset}>Reset</button></p>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
